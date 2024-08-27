41cd907e39730effd063f4cb87be48e46f31cb01
Ave ceena17 <mingave11@gmail.com>
2024-08-27 08:51:12 -0700

    memperbaiki style exce

--- src/main/java/com/example/absensireact/controller/UserController.java
+++ src/main/java/com/example/absensireact/controller/UserController.java
@@ -10,12 +10,8 @@ import com.example.absensireact.exception.ResponseHelper;
 import com.example.absensireact.exel.ExcelDataKaryawan;
 import com.example.absensireact.exel.ExcelDataSiswa;
 import com.example.absensireact.exel.ImportDataKaryawan;
-import com.example.absensireact.exel.ImportSiswa;
-import com.example.absensireact.model.Admin;
-import com.example.absensireact.model.Organisasi;
 import com.example.absensireact.model.User;
 import com.example.absensireact.repository.AdminRepository;
-import com.example.absensireact.repository.KelasRepository;
 import com.example.absensireact.repository.OrganisasiRepository;
 import com.example.absensireact.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +24,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 
 @CrossOrigin(origins = "*")
@@ -50,20 +44,17 @@ public class UserController {
     @Autowired
     private AdminRepository adminRepository;
 
-    @Autowired
-    private KelasRepository kelasRepository;
+//    @Autowired
+//    private KelasRepository kelasRepository;
 
-    @Autowired
-    private ImportSiswa importSiswa;
-
-    @Autowired
-    private ExcelDataSiswa excelDataSiswa;
-
-    @Autowired
-    private ExcelDataKaryawan excelDataKaryawan;
+//    @Autowired
+//    private ImportSiswa importSiswa;
 
     @Autowired
     private ImportDataKaryawan importDataKaryawan;
+
+    @Autowired
+    private ExcelDataSiswa excelDataSiswa;
 
     @Autowired
     private AppConfig appConfig;
@@ -109,37 +100,37 @@ public class UserController {
         return userImpl.GetAllKaryawanByIdAdmin(idAdmin);
     }
 
-    @PostMapping("/user/tambahkaryawan/{idAdmin}")
-    public ResponseEntity<User> tambahKaryawan(
-            @RequestBody UserDTO userDTO,
-            @PathVariable Long idAdmin,
-            @RequestParam Long idOrganisasi,
-            @RequestParam Long idOrangTua,
-            @RequestParam Long idShift) {
-        try {
-            User savedUser = userImpl.Tambahkaryawan(userDTO, idAdmin, idOrganisasi, idShift, idOrangTua);
-            return ResponseEntity.ok(savedUser);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
+//    @PostMapping("/user/tambahkaryawan/{idAdmin}")
+//    public ResponseEntity<User> tambahKaryawan(
+//            @RequestBody UserDTO userDTO,
+//            @PathVariable Long idAdmin,
+//            @RequestParam Long idOrganisasi,
+//            @RequestParam Long idOrangTua,
+//            @RequestParam Long idShift) {
+//        try {
+//            User savedUser = userImpl.Tambahkaryawan(userDTO, idAdmin, idOrganisasi, idShift, idOrangTua);
+//            return ResponseEntity.ok(savedUser);
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//    }
 
-    @PostMapping("/user/tambahuser/byAdmin/{idAdmin}/byKelas")
-    public ResponseEntity<User> tambahUserKelas(
-            @RequestBody UserDTO userDTO,
-            @PathVariable Long idAdmin,
-            @RequestParam Long idOrganisasi,
-            @RequestParam Long idOrangTua,
-            @RequestParam Long idShift,
-            @RequestParam Long idKelas
-    ) {
-        try {
-            User savedUser = userImpl.TambahUserKelas(userDTO, idAdmin, idOrganisasi, idShift, idOrangTua, idKelas);
-            return ResponseEntity.ok(savedUser);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
+//    @PostMapping("/user/tambahuser/byAdmin/{idAdmin}/byKelas")
+//    public ResponseEntity<User> tambahUserKelas(
+//            @RequestBody UserDTO userDTO,
+//            @PathVariable Long idAdmin,
+//            @RequestParam Long idOrganisasi,
+//            @RequestParam Long idOrangTua,
+//            @RequestParam Long idShift,
+//            @RequestParam Long idKelas
+//    ) {
+//        try {
+//            User savedUser = userImpl.TambahUserKelas(userDTO, idAdmin, idOrganisasi, idShift, idOrangTua, idKelas);
+//            return ResponseEntity.ok(savedUser);
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//    }
 
     @PutMapping("/user/edit-email-username/{id}")
     public ResponseEntity<User> editemailusername(@PathVariable Long id, @RequestBody User updateUser) {
@@ -212,38 +203,38 @@ public class UserController {
         }
     }
 
-    @PutMapping("/user/editBYSuper/{id}")
-    public ResponseEntity<User> editUserBySuper(@PathVariable Long id,
-                                                @RequestParam Long idShift,
-                                                @RequestParam Long idOrangTua,
-                                                @RequestParam Long idKelas,
-                                                @RequestBody User user) {
-        try {
-            User updatedUser = userImpl.EditUserBySuper(id, idShift, idOrangTua, idKelas, user);
-            return ResponseEntity.ok(updatedUser);
-        } catch (NotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
-        }
-    }
+//    @PutMapping("/user/editBYSuper/{id}")
+//    public ResponseEntity<User> editUserBySuper(@PathVariable Long id,
+//                                                @RequestParam Long idShift,
+//                                                @RequestParam Long idOrangTua,
+//                                                @RequestParam Long idKelas,
+//                                                @RequestBody User user) {
+//        try {
+//            User updatedUser = userImpl.EditUserBySuper(id, idShift, idOrangTua, idKelas, user);
+//            return ResponseEntity.ok(updatedUser);
+//        } catch (NotFoundException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+//        }
+//    }
 
 
-    @PutMapping("/user/edit-kar/{id}")
-    public ResponseEntity<User> editUser(
-            @PathVariable("id") Long id,
-            @RequestParam(required = false) Long idJabatan,
-            @RequestParam(required = false) Long idShift,
-            @RequestParam(required = false) Long idOrangTua,
-            @RequestParam(required = false) Long idKelas,
-            @RequestBody UserDTO updatedUserDTO) {
-        try {
-            User editedUser = userImpl.editUsernameJabatanShift(id, idJabatan, idShift, idOrangTua, idKelas, updatedUserDTO);
-            return ResponseEntity.ok(editedUser);
-        } catch (NotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
+    //    @PutMapping("/user/edit-kar/{id}")
+//    public ResponseEntity<User> editUser(
+//            @PathVariable("id") Long id,
+//            @RequestParam(required = false) Long idJabatan,
+//            @RequestParam(required = false) Long idShift,
+//            @RequestParam(required = false) Long idOrangTua,
+//            @RequestParam(required = false) Long idKelas,
+//            @RequestBody UserDTO updatedUserDTO) {
+//        try {
+//            User editedUser = userImpl.editUsernameJabatanShift(id, idJabatan, idShift, idOrangTua, idKelas, updatedUserDTO);
+//            return ResponseEntity.ok(editedUser);
+//        } catch (NotFoundException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        }
+//    }
     @PutMapping("/user/editFotoBY/{id}")
     public ResponseEntity<User> editFotoUser(@PathVariable Long id, @RequestPart("image") MultipartFile image) {
         try {
@@ -277,14 +268,14 @@ public class UserController {
         }
     }
 
-    @GetMapping("/user/by-kelas/{idKelas}")
-    public ResponseEntity<List<User>> getUserByIdKelas(@PathVariable Long idKelas) {
-        List<User> users = userImpl.getUsersByIdKelas(idKelas);
-        if (users.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(users);
-    }
+//    @GetMapping("/user/by-kelas/{idKelas}")
+//    public ResponseEntity<List<User>> getUserByIdKelas(@PathVariable Long idKelas) {
+//        List<User> users = userImpl.getUsersByIdKelas(idKelas);
+//        if (users.isEmpty()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(users);
+//    }
 
     @GetMapping("/user/export-data-siswa/{idAdmin}")
     public ResponseEntity<Void> exportDataSiswa(@PathVariable Long idAdmin, HttpServletResponse response) {
@@ -309,47 +300,47 @@ public class UserController {
     }
 
     //    @PostMapping("/import/data-siswa/admin/{adminId}/jabatan/{idJabatan}/walimurid/{idOrangTua}/shift/{idShift}/organisasi/{idOrganisasi}")
-    @PostMapping("/import/data-siswa/admin/{adminId}")
-    public ResponseEntity<String> importUser(@RequestPart("file") MultipartFile file, @PathVariable Long adminId) {
-        return adminRepository.findById(adminId).map(admin -> {
-            try {
-                importSiswa.importUser(file, admin);
-                return ResponseEntity.ok("Import berhasil!");
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Terjadi kesalahan saat mengimpor data: " + e.getMessage());
-            }
-        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("SuperAdmin not found"));
-    }
+//    @PostMapping("/import/data-siswa/admin/{adminId}")
+//    public ResponseEntity<String> importUser(@RequestPart("file") MultipartFile file, @PathVariable Long adminId) {
+//        return adminRepository.findById(adminId).map(admin -> {
+//            try {
+//                importSiswa.importUser(file, admin);
+//                return ResponseEntity.ok("Import berhasil!");
+//            } catch (Exception e) {
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Terjadi kesalahan saat mengimpor data: " + e.getMessage());
+//            }
+//        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("SuperAdmin not found"));
+//    }
 
 
-    @GetMapping("/user/export-data-siswa/{idAdmin}/perkelas/{KlasId}")
-    public ResponseEntity<Void> exportDataSiswa(@PathVariable Long idAdmin, @PathVariable Long KlasId, HttpServletResponse response) {
-        try {
-            excelDataSiswa.exportDataSiswaperKelas(idAdmin, KlasId, response);
-            return ResponseEntity.ok().build();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).build();
-        }
-    }
+//    @GetMapping("/user/export-data-siswa/{idAdmin}/perkelas/{KlasId}")
+//    public ResponseEntity<Void> exportDataSiswa(@PathVariable Long idAdmin, @PathVariable Long KlasId, HttpServletResponse response) {
+//        try {
+//            excelDataSiswa.exportDataSiswaperKelas(idAdmin, KlasId, response);
+//            return ResponseEntity.ok().build();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return ResponseEntity.status(500).build();
+//        }
+//    }
 
     //    @PostMapping("/import/data-siswa/admin/{adminId}/jabatan/{idJabatan}/walimurid/{idOrangTua}/shift/{idShift}/organisasi/{idOrganisasi}")
-    @PostMapping("/import/data-siswa/admin/{adminId}/kelas/{kelasId}")
-    public ResponseEntity<String> importUserperKelas(@RequestPart("file") MultipartFile file, @PathVariable Long adminId, @PathVariable Long kelasId) {
-        return adminRepository.findById(adminId).map(admin ->
-                kelasRepository.findById(kelasId).map(kelas -> {
-                    try {
-                        importSiswa.importUserperKelas(file, admin, kelas);
-                        return ResponseEntity.ok("Import berhasil!");
-                    } catch (Exception e) {
-                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                .body("Terjadi kesalahan saat mengimpor data: " + e.getMessage());
-                    }
-                }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body("Kelas dengan ID " + kelasId + " tidak ditemukan"))
-        ).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Admin dengan ID " + adminId + " tidak ditemukan"));
-    }
+//    @PostMapping("/import/data-siswa/admin/{adminId}/kelas/{kelasId}")
+//    public ResponseEntity<String> importUserperKelas(@RequestPart("file") MultipartFile file, @PathVariable Long adminId, @PathVariable Long kelasId) {
+//        return adminRepository.findById(adminId).map(admin ->
+//                kelasRepository.findById(kelasId).map(kelas -> {
+//                    try {
+//                        importSiswa.importUserperKelas(file, admin, kelas);
+//                        return ResponseEntity.ok("Import berhasil!");
+//                    } catch (Exception e) {
+//                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                                .body("Terjadi kesalahan saat mengimpor data: " + e.getMessage());
+//                    }
+//                }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                        .body("Kelas dengan ID " + kelasId + " tidak ditemukan"))
+//        ).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                .body("Admin dengan ID " + adminId + " tidak ditemukan"));
+//    }
 
     @GetMapping("/user/export/{idAdmin}")
     public ResponseEntity<InputStreamResource> exportKaryawanByAdmin(@PathVariable Long idAdmin) {
@@ -389,5 +380,7 @@ public class UserController {
             }
         }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("SuperAdmin not found"));
     }
+
+
 
 }
